5a06c391a75b31887160ddfc11cdd7051cf4c42e
Ifrat <ifrat1998@gmail.com>
2024-10-30 19:03:58 +0500

    homework variable and data types

---
+++ homework to variable and data types/js/script.js
@@ -0,0 +1,19 @@
+let name = "Ali"
+
+console.log("Аты:" + name + " | Типы: " + typeof name);
+
+var age = 25
+
+console.log("Жасы:" + age + " | Типы: " + typeof age );
+
+const isStudent = true
+
+console.log("Студент пе?: " + isStudent +  " | Типы: " + typeof isStudent);
+
+if(true){
+    let age = 25
+    
+    console.log("Жасы (жол ретінде:) " + age + " |Типы: " +typeof age.toString(age) );
+}
+
+
