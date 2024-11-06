3bda183408ed3aa65f739c9fde8978417b28b04c
Ifrat <ifrat1998@gmail.com>
2024-11-06 08:59:05 +0500

    inputoutput

---
+++ homework input output/java/java.js
@@ -0,0 +1,31 @@
+/*  
+ let course = prompt("Курсініңізді енгізіңіз:   1/2/3/4")
+let isAlmaty = prompt("Келген жеріңіз Алматыма?  yes / no")
+
+
+if(course<=3 && course<=4 || isAlmaty == "no"){
+    alert("Құттықтаймын! Сізге жатақханадан орын беріледі")
+}
+else{
+    alert("Кешіріңіз! Сізге  жатақханадан орын берілмейді")
+} 
+ */
+
+
+
+alert("Ойын шарты: Дұрыс жауап берілген әр сқраққа 1 ұпай беріледі." + "Cұрақ қойылады жауапты енгізіңіз")
+let surak1 = prompt("Қазақстан тәуелсіздігін қашан алды?")
+let surak2 = prompt("Қай қалада оқып жатырсыз?(Almaty + 1 / Басқасы -1)")
+let score = 0
+if(surak1== 1991  ){
+    score + +
+}  
+
+if(surak2=="Almaty"){
+    score + +
+}
+    if(score==0){
+        alert("Сізге ұпай қосылмады"  )
+} else{
+    alert("Siz eki upay zhynadynyz: 2"  )
+}
