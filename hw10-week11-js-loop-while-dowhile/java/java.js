6d47abf3f58aa252619ad0941dbf954e82efde23
Ifrat <ifrat1998@gmail.com>
2024-11-16 20:48:33 +0500

    while1 commit

--- hw10-week11-js-loop-while-dowhile/java/java.js
+++ hw10-week11-js-loop-while-dowhile/java/java.js
@@ -1,18 +1,18 @@
-let numStudent = +prompt("Студенттер санын енгізіңіз: ");
-let a = 0;
-let b = 1;
+// let numStudent = +prompt("Студенттер санын енгізіңіз: ");
+// let a = 0;
+// let b = 1;
 
-do {
-    let grade = +prompt(b+" студенттің бағасын енгізіңіз: ");
-    a += grade;
-    b++;
-} while (b <= numStudent);
-
-
-let ortasy = a / numStudent;
+// do {
+//     let grade = +prompt(b+" студенттің бағасын енгізіңіз: ");
+//     a += grade;
+//     b++;
+// } while (b <= numStudent);
 
 
-alert("Студенттердің бағаларының орташа мәні: " + ortasy);
+// let ortasy = a / numStudent;
+
+
+// alert("Студенттердің бағаларының орташа мәні: " + ortasy);
 
 
 
@@ -23,14 +23,26 @@ alert("Студенттердің бағаларының орташа мәні: 
 
 // while (san <= student) {
 //     let sum = +prompt(san+"студенттің бағасын енгізіңіз: ");
-//     i += sum;
+//     i +=  sum;
 //     san++;
-// }
+// }    
 
 
 // let x = i / student;
 
 
 // alert("Студенттердің бағаларының орташа мәні:"+x);
+
+
+let san = +prompt("Кез келген оң сан енгізіңіз:");
+let i = 5
+while(i<=100){
+    if(san % 5 == 0 && i % 5 == 0){
+        alert("Ең үлкен 5-ке бөлінетін сан : " + san +'\n' +
+            "Ең кіші 5-ке бөлінетін сан: " + i
+        )
+    }
+}
+
 
 
