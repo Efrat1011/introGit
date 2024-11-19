2768072375075498c2fa598a322e41e2677393b7
Ifrat <ifrat1998@gmail.com>
2024-11-19 08:38:02 +0500

    function commit

---
+++ hw11-week12-js-function/java/java.js
@@ -0,0 +1,20 @@
+
+
+
+let txt1 =prompt("1-ші сөзді жаз").toLowerCase() 
+let txt2 =prompt("2-ші сөзді жаз").toLowerCase() 
+ 
+  
+if (txt1==txt2) { 
+        function sozder() { 
+               txt1(txt1) 
+               txt2(txt2) 
+        } 
+       alert("Сөздер бірдей жазылған")   
+        
+} 
+  
+else{ 
+        alert("Сөздер әртүрлі жазылған") 
+       
+}
