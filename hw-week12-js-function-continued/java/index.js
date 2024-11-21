d6615718b63d12f62c9f6e849c89cc162871c61e
Ifrat <ifrat1998@gmail.com>
2024-11-21 08:33:28 +0500

    advenced function

--- hw-week12-js-function-continued/java/index.js
+++ hw-week12-js-function-continued/java/index.js
@@ -1,4 +1,4 @@
-
+/* 
 
  let student = prompt("Студент атын жазыңыз: ")
 
@@ -22,4 +22,53 @@
      }
  }
 
- alert( kobeitu(student,language))
+ alert( kobeitu(student,language)) 
+ */
+
+
+
+
+
+ 
+/* 
+ let student = prompt("Студент атын жазыңыз: ")
+
+ let language = prompt("Қай тілде жауап берсін: ")
+
+ function english(student, language) {
+    if( language == "english"){
+        
+         return student + " Hello!  How are you?";
+
+     }else{
+        "Error"
+     }
+    
+}
+let angl = english(student,language)
+function russion(student, language) {
+    if( language== "russion"){
+        
+         return student + " Привет!  как дела?";
+
+     }else{
+        "Error"
+     }
+     
+}
+let orys = russion(student, language)
+
+function kazakh(student, language) {
+    if( language == "kazakh"){
+        
+         return student + " Сәлем!  Қалайсың?";
+
+     }else{
+        "Error"
+     }
+ 
+}
+let kazak = kazakh(student, language)
+
+alert( english(orys,kazak))
+ */
