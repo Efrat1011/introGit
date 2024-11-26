c24f53196ea4a10b3fa7a480638ba86f38adcf21
Ifrat <ifrat1998@gmail.com>
2024-11-26 20:53:29 +0500

    array commit

---
+++ hw-week13-js-array/java/java.js
@@ -0,0 +1,23 @@
+let footballPalayer = ["Ronaldo","Zidan","Neymar","Benzema"]
+
+console.log(footballPalayer);
+
+footballPalayer[1] = "Messi"
+
+
+
+footballPalayer[4] = "Lamin Yamal"
+footballPalayer[5] = "Pedri"
+footballPalayer[6] = "Gavi"
+
+console.log(footballPalayer);
+
+if (footballPalayer.length>5) {
+    console.log("Бестен көп");
+}else{
+    console.log("Бестен аз");
+    
+}
+
+
+
