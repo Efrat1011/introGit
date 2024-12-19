1fe4ea2c5fd1262ae45cc2b53ae2649aed03f52a
Ifrat <ifrat1998@gmail.com>
2024-12-19 08:44:42 +0500

    manipulating cont

--- hw-week16-js-manipulation con/js/script.js
+++ hw-week16-js-manipulation con/js/script.js
@@ -1,3 +1,8 @@
+// let form = document.getElementById('form')
+// let addBox = document.getElementById('addBox')
+// let searchBox = document.getElementById('searchBox')
+
+
 let addBtn = document.getElementById('addBtn')
 let listBox = document.getElementById('list-box')
 
@@ -39,3 +44,49 @@ searchBtn.addEventListener('click',(e)=>{
     });
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+function createNewList(newToDoItem){
+    let newDiv = document.createElement('div')
+    let itemName = document.createElement('p')
+    let deleteButton =document.createElement('button')
+    newDiv.className = "newDiv"
+    newDiv.style.backgroundColor = 'cadetblue'
+    newDiv.style.borderRadius = 15+"px"
+    newDiv.style.textAlign = "center"
+    newDiv.style.display = "flex"
+    newDiv.style.justifyContent = "space-between"
+    newDiv.style.marginTop = 50 + "px" 
+    newDiv.style.paddingLeft = 50 + "px"
+    
+    
+
+    deleteButton.className = "deleteBtn"
+    deleteButton.style.width = 100 +"px"
+    deleteButton.style.borderRadius = 12 + "px"
+    deleteButton.style.backgroundColor = "darkviolet"
+
+    itemName.textContent = newToDoItem
+    deleteButton.textContent = 'Өшір'
+    
+    deleteButton.addEventListener('click',() => {
+        listBox.removeChild(newDiv)
+    })
+
+
+
+    newDiv.appendChild(itemName)
+    newDiv.appendChild(deleteButton)
+
+    return newDiv
+}
