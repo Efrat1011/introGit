c0ffd30eacabb710f1e298806a213e09374c49c9
Ifrat <ifrat1998@gmail.com>
2025-01-20 15:17:11 +0500

    asicns COD

---
+++ hw-week19-asincsCOD/index.js
@@ -0,0 +1,19 @@
+let countdownInterval;
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    clearInterval(countdownInterval);
+
+    const message = document.getElementById("message");
+    let timeLeft = 5;
+    message.textContent = `Хабарлама аяқталуына ${timeLeft}`;
+
+    countdownInterval = setInterval(() => {
+        timeLeft--;
+        if (timeLeft > 0) {
+            message.textContent = `Хабарлама аяқталуына ${timeLeft}`;
+        } else {
+            clearInterval(countdownInterval);
+            message.textContent = "Уақытыңыз аяқталды! ";
+        }
+    }, 1000);
+});
